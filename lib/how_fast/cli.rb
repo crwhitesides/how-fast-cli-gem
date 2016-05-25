e530accad6a179f3c8a69d347ce592566d9614cf
Christian Whitesides <whitesidescr@gmail.com>
2016-05-25 12:11:56 -0600

    Create #print_phrase method and clean up code

--- lib/how_fast/cli.rb
+++ lib/how_fast/cli.rb
@@ -1,28 +1,37 @@
 class HowFast::CLI
 
   def call
-    puts "Beep, boop, beep. Beginning speed test... "
+    puts "Beep, boop, beep. Beginning speed test..."
 
     session = Capybara.current_session
     session.visit("https://fast.com/")
 
-    phrases = [
-      "Hang tight, we're almost there...", 
-      "Still cruisin'...", 
-      "Patience is a virtue (especially when waiting to know your internet speed).", 
-      "There's light at the end of the tunnel...",
-      "Chug, chug, chug...",
-      "So, ummmm, how's your day going?"
-    ]
-
-    while session.has_css?('.speed-results-container.succeeded') == false
-      puts phrases.sample
-      sleep 10
+    while !session.has_css?('.speed-results-container.succeeded')
+      print_phrase
+      sleep 1
     end
 
     speed = session.find_by_id('speed-value').text
     units = session.find_by_id('speed-units').text
-    puts "Your speed is: #{speed} #{units}"
+    
+    puts ""
+    puts "YOUR SPEED IS: #{speed} #{units}"
+  end
+
+  def print_phrase
+    phrases = [
+      "Hang tight, we're almost there...", 
+      "Still movin'...", 
+      "Patience is a virtue...", 
+      "There's light at the end of the tunnel...",
+      "Chug, chug, chug...",
+      "So, how's your day going?",
+      "Life is too short to have slow internet.",
+      "Making progress...",
+      "Wait for it..."
+    ]
+
+    puts phrases.sample
   end
   
 end
